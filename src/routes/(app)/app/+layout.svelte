ff231e920fa36899859595eefa81926fc6fca3a3
andymarsden <46504022+andymarsden@users.noreply.github.com>
2026-05-16 18:36:23 +0000

    ui shell update

---
+++ src/routes/(app)/app/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import * as Sidebar from '$lib/components/ui/sid$lib/components/shadcn/sidebar/index.js= $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>STAT</title>
+</svelte:head>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		{@render children()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
---
+++ src/routes/(app)/app/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+     import AppHeader from "$lib/components/app-header.svelte";
+</script>
+<AppHeader
+    crumbs={[
+        { label: "Admin", href: "/app" },
+        { label: "Sandbox", href: "/app/sandbox" },
+    ]}
+    currentPage="Porto"
+/>
