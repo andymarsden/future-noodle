5d6a144e848a5ccd819872ba35a39e363b8a3e4e
jamesfredio <Jamesfrederick14@icloud.com>
2026-06-26 13:26:21 +0000

    flow and engine

---
+++ src/lib/chat/flows/barnsley.js
@@ -0,0 +1,11 @@
+export const barnsleyFlow = {
+
+    id: "barnsley",
+    name: "Barnsley Flow",
+    description: "A Barnsley flow",
+    index: 0,
+    name: "Ask Question",
+    question: "What can we help you with today?",
+    command: "util.getAdvice",
+    
+        }
--- src/lib/chat/flows/engine.js
+++ src/lib/chat/flows/engine.js
@@ -4,11 +4,13 @@ import { rugbyFlow } from "./rugby";
 import { qriosWelcomeFlow } from "./qrios_welcome";
 import { qriosFormFlow } from "./qrios";
 import { qriosFormSpanishFlow } from "./qrios_spanish";
+import { barnsleyFlow } from "./barnsley";
 
 export const flowRegistry = {
     "onboard": onboardFlow,
     "rugby" : rugbyFlow,
     "qrios_welcome": qriosWelcomeFlow,
     "qrios_form": qriosFormFlow,
-    "qrios_form_spanish": qriosFormSpanishFlow
+    "qrios_form_spanish": qriosFormSpanishFlow,
+    "barnsley": barnsleyFlow
 };
